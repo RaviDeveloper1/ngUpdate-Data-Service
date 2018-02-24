3728a6d71f357b22683d7e56827c351af2bd2b8a
RaviDeveloper1 <aravi.us80@gmail.com>
2018-02-24 14:10:29 -0600

    addes observable

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class UsersService {
+  private user = new BehaviorSubject<string>('josh');
+  cast = this.user.asObservable();
 
   constructor() { }
 
+    editUser(newUser) {
+      this.user.next(newUser);
+    }
 }
+
